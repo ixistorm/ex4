9b66d29979eff4b7ae0048684d8b866f2369561f
KobNoi <59143425@g.cmru.ac.th>
2019-07-14 12:13:02 +0700

    jiji

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
-router.get("/play/:num1/:num2", function(req, res, next) {
-  //console.log(req.params)
-  var num1 = req.params.num1;
-  var num2 = req.params.num2;
-  var sum = parseInt(num1) + parseInt(num2);
-  res.send("ผลรวมของตัวเลข 2 จำนวนคือ " + sum);
- });
- 
+var connect = require("../config/database");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+ res.render("index");
 });
 
-router.get('/create', function(req, res, next) {
-  res.render('create', { title: 'Express' });
+router.get("/create", function(req, res, next) {
+ var query = "SELECT * FROM categories";
+ connect.query(query, function(error, result) {
+   console.log(result);
+   res.render("create");
+ });
 });
 
-router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+router.get("/login", function(req, res, next) {
+ res.render("login");
 });
-
-
-
 
 module.exports = router;
+
+
